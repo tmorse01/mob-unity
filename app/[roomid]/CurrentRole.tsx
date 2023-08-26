2cf86af652ecb7ac6f76ee74568699f1f73d9a9b
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-08-26 11:06:02 -0700

    current role display done

--- app/[roomid]/CurrentRole.tsx
+++ app/[roomid]/CurrentRole.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 
 interface CurrentRolesProps {
-  currentDriver: string;
-  currentNavigator: string;
+  driver: string | undefined;
+  navigator: string | undefined;
+  facilitator: string | undefined;
+  mob: string[] | undefined;
 }
 
 const CurrentRoles: React.FC<CurrentRolesProps> = ({
-  currentDriver,
-  currentNavigator,
+  driver,
+  navigator,
+  facilitator,
+  mob,
 }) => {
   return (
     <div className="flex flex-col items-center space-y-2">
-      <h2 className="text-lg font-semibold">Current Driver and Navigator</h2>
       <div className="text-center">
         <p className="text-gray-600">Driver:</p>
-        <p className="text-xl font-bold">{currentDriver}</p>
+        <p className="text-xl font-bold">{driver}</p>
       </div>
       <div className="text-center">
         <p className="text-gray-600">Navigator:</p>
-        <p className="text-xl font-bold">{currentNavigator}</p>
+        <p className="text-xl font-bold">{navigator}</p>
+      </div>
+      {facilitator && (
+        <div className="text-center">
+          <p className="text-gray-600">Facilitator:</p>
+          <p className="text-xl font-bold">{facilitator}</p>
+        </div>
+      )}
+      <div className="flex flex-row gap-4">
+        <p className="text-gray-600">Mobsters:</p>
+        {mob?.map((mobster, i) => (
+          <div key={mobster + i} className="text-center">
+            <p className="text-xl font-bold">{mobster}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- app/[roomid]/page.tsx
+++ app/[roomid]/page.tsx
@@ -3,12 +3,26 @@ import { useState } from "react";
 import GoalsSection from "./GoalSection";
 import TeamSection from "./TeamSection";
 import Timer from "./Timer";
+import CurrentRoles from "./CurrentRole";
+
+type Roles = {
+  driver: string | undefined;
+  navigator: string | undefined;
+  facilitator: string | undefined;
+  mob: string[] | undefined;
+};
 
 const Room = () => {
   const [isRunning, setIsRunning] = useState(false);
   const [teamMembers, setTeamMembers] = useState<string[]>([]);
   const [goals, setGoals] = useState<string[]>([]);
   const [turnDuration, setTurnDuration] = useState<number>(420);
+  const [currentRoles, setCurrentRoles] = useState<Roles>({
+    driver: "Taylor",
+    navigator: "Steve",
+    facilitator: "Emmaneul",
+    mob: ["Paul", "Peter", "Mary"],
+  });
 
   const handleStart = () => {
     setIsRunning(true);
@@ -31,16 +45,26 @@ const Room = () => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 min-h-screen bg-base-300">
-      <Timer
-        duration={turnDuration}
-        onStart={handleStart}
-        onPause={handlePause}
-        onReset={onReset}
-        isRunning={isRunning}
-      />
-      <TeamSection teamMembers={teamMembers} onAddMember={handleAddMember} />
-      <GoalsSection goals={goals} onToggleGoal={handleToggleGoal} />
+    <div className="flex  gap-4 min-h-screen bg-base-300">
+      <div className="w-1/2 p-4">
+        <Timer
+          duration={turnDuration}
+          onStart={handleStart}
+          onPause={handlePause}
+          onReset={onReset}
+          isRunning={isRunning}
+        />
+        <CurrentRoles
+          driver={currentRoles.driver}
+          navigator={currentRoles.navigator}
+          facilitator={currentRoles.facilitator}
+          mob={currentRoles.mob}
+        />
+      </div>
+      <div className="w-1/2 p-4">
+        <TeamSection teamMembers={teamMembers} onAddMember={handleAddMember} />
+        <GoalsSection goals={goals} onToggleGoal={handleToggleGoal} />
+      </div>
     </div>
   );
 };
