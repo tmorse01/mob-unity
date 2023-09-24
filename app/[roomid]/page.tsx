a5f85803810c0139b697857d559c99a9bc6e3521
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2023-09-23 20:59:31 -0700

    testing axios

--- app/[roomid]/page.tsx
+++ app/[roomid]/page.tsx
@@ -1,5 +1,6 @@
 import { defaultRoom } from "@/types/room";
 import Room from "./Room";
+import axios from "axios";
 
 // const initRoles = (teamMembers: string[]) => {
 //   const shuffledMembers = [...teamMembers].sort(() => Math.random() - 0.5);
@@ -14,25 +15,32 @@ import Room from "./Room";
 
 async function getRoomData(roomId: string) {
   // TODO implement smart caching with on demand revalidation
-  return fetch(process.env.NEXT_PUBLIC_URL + `/api/rooms/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ action: "getRoom", roomid: roomId }),
-    cache: "no-store",
-  })
-    .then((response) => response.json())
+  return axios
+    .post(process.env.NEXT_PUBLIC_URL + `/api/rooms/`, {
+      action: "getRoom",
+      roomid: roomId,
+    })
+    .then((response) => {
+      console.log("Response: ", response.data);
+      return response.data.data;
+    })
     .catch((error) => {
       console.log(error);
       return defaultRoom;
     });
+
+  // return fetch(process.env.NEXT_PUBLIC_URL + `/api/rooms/`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({ action: "getRoom", roomid: roomId }),
+  // })
 }
 
 const RoomPage = async ({ params }: { params: { roomid: string } }) => {
   const roomId = params.roomid;
-  const response = await getRoomData(roomId);
-  const roomData = response.data;
+  const roomData = await getRoomData(roomId);
   if (roomData === undefined) return <div>Room not found</div>;
   return <Room roomData={roomData} roomId={roomId} />;
 };
